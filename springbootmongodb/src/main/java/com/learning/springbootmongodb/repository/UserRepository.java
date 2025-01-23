9d8d8285a924b7c254c52b85914e8f416dd2ef0d
amit <amit.dahiya.j@gmail.com>
2025-01-23 04:58:59 +0000

    added springboot mongodb crud

---
+++ springbootmongodb/src/main/java/com/learning/springbootmongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.learning.springbootmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.learning.springbootmongodb.model.User;
+
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
