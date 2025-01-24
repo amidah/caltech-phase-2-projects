6743416e8e179b5b19f9eeb086fc8ff222e6de48
amit <amit.dahiya.j@gmail.com>
2025-01-24 05:02:11 +0000

    added spring cloud client example

---
+++ springcloudconfigclientdemo/src/main/java/com/learning/springcloudconfigclientdemo/SpringcloudconfigclientdemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springcloudconfigclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconfigclientdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigclientdemoApplication.class, args);
+	}
+
+}
