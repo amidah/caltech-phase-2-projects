4767164dbc5ee23b47d6d772ccdbb7acb627e519
amit <amit.dahiya.j@gmail.com>
2025-01-31 04:41:34 +0000

    added SOAP WS Example

---
+++ SOAPWSDemo/src/main/java/com/learning/SOAPWSDemo/SoapwsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.SOAPWSDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwsDemoApplication.class, args);
+	}
+
+}
